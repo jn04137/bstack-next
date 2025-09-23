1ddb67e02e9deaf62b54363246ed0860ce9164c1
Jonathan Nguyen <jnganguyen3@gmail.com>
2025-09-23 18:02:31 -0500

    working on team page

--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -4,7 +4,8 @@ import Link from 'next/link';
 interface Team {
     id: number;
     teamName: string;
-    details: string;
+    teamDetails: string;
+    teamNanoId: string;
 }
 
 export default async function Page() {
@@ -28,13 +29,13 @@ function TeamCard({team}: {
     console.log(team)
     return (
         <div className="p-5 rounded bg-white/5 items-center">
-            <Link href={`/teams/team/${team.id}`}>
+            <Link href={`/teams/team/${team.teamNanoId}`}>
                 <div className='w-full flex divide-x items-center'>
                     <div className="w-1/3">
                         <h2 className="text-2xl font-bold">{team.teamName}</h2>
                     </div>
                     <div className="px-2 w-2/3 flex-grow">
-                        <p>{team.details}</p>
+                        <p>{team.teamDetails}</p>
                     </div>
                 </div>
             </Link>
--- app/teams/team/[team_name]/page.tsx
+++ app/teams/team/[team_name]/page.tsx
@@ -1,11 +1,113 @@
 
+type ITeam = {
+    teamId: number
+    teamName: string
+    teamNanoId: string
+    teamDetails: string
+    teamOwnerUsername: string
+}
+
 export default async function Page({
     params,
 }: {
     params: Promise<{ team_name: string }>
 }) {
     const { team_name } = await params
+    let team: ITeam = await fetchTeamData(team_name)
     return (
-        <div> Current team: {team_name}</div>
+        <div className="flex justify-center">
+            <div className="w-1/2 space-y-2">
+                <TeamCard teamName={team.teamName} teamDetails={team.teamDetails}/>
+                <AchievmentsSection/>
+                <PlayersSection/>
+                <CommentsSection/>
+            </div>
+        </div>
     )
 }
+
+async function fetchTeamData(teamNanoId: string): Promise<ITeam> {
+    const url = `${process.env.NEXT_PUBLIC_HOST}/team/${teamNanoId}`
+    const response = await fetch(url)
+    if(!response.ok) {
+        console.log("Failed to query team: ", response.status)
+    }
+    const data: ITeam = await response.json()
+    return data
+}
+
+function TeamCard({teamName, teamDetails}: {
+    teamName: string,
+    teamDetails: string
+}) {
+    return(
+        <div className="rounded bg-white/5 p-5 space-y-2">
+            <div>
+                <h1 className="text-2xl font-bold">{teamName}</h1>
+            </div>
+            <div>
+                <p>{teamDetails}</p>
+            </div>
+            <div>
+                Leagues<br/>
+                ESEA<br/>
+            </div>
+        </div>
+    )
+}
+
+function PlayerCard() {
+    return(
+        <div className="bg-white/5 rounded p-5 grow">
+            <div>
+                <h2>Player Name</h2>
+            </div>
+            <div>
+                <h3>FaceIt Rank</h3>
+            </div>
+        </div>
+    )
+}
+
+function PlayersSection() {
+    return(
+        <div className="bg-white/5 rounded grow p-5 space-y-2">
+            <h2 className="text-xl">Players</h2>
+            <div className="flex space-x-2">
+                <PlayerCard/>
+                <PlayerCard/>
+                <PlayerCard/>
+                <PlayerCard/>
+                <PlayerCard/>
+            </div>
+        </div>
+    )
+}
+
+function AchievmentsSection() {
+    return(
+        <div className="p-5 bg-white/5 rounded">
+            <div>
+                <h1 className="text-xl">Achievements</h1>
+            </div>
+            <div>
+                <p></p>
+            </div>
+        </div>
+    )
+}
+
+function CommentsSection() {
+    return(
+        <div className="p-5 bg-white/5 rounded">
+            <div>
+                <h1 className="text-xl">Comments</h1>
+            </div>
+            <div>
+                <p></p>
+            </div>
+        </div>
+    )
+}
+
+
